3d5e6b14a0cee7a13bb071cc1191f27de4701728
Christian Häußler (Windows) <chrissx@chrissx.ga>
2020-02-04 20:18:25 +0100

    Started writing a frontend.

---
+++ frontend_client/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontend_client
+{
+    class Program
+    {
+        struct program
+        {
+            public string src;
+            public string dst;
+            public string exe;
+
+            public program(string src, string dst, string exe)
+            {
+                this.src = src;
+                this.dst = dst;
+                this.exe = exe;
+            }
+        }
+
+        static void CopyDir(DirectoryInfo src, DirectoryInfo dst)
+        {
+            foreach (DirectoryInfo dir in src.GetDirectories())
+                CopyDir(dir, dst.CreateSubdirectory(dir.Name));
+            foreach (FileInfo file in src.GetFiles())
+                file.CopyTo(Path.Combine(dst.FullName, file.Name));
+        }
+
+        static void CopyProgram(program p)
+        {
+            Directory.CreateDirectory(p.dst);
+            CopyDir(new DirectoryInfo(p.src), new DirectoryInfo(p.dst));
+            File.Move(Path.Combine(p.dst, p.exe), Path.Combine(p.dst, "firefox.exe"));
+        }
+
+        static void CopyPrograms(program[] ps)
+        {
+            foreach (program p in ps)
+                CopyProgram(p);
+        }
+
+        static void Init()
+        {
+            CopyPrograms(null);
+        }
+
+        static void Tick()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            Init();
+
+            while (true)
+                Tick();
+        }
+    }
+}
