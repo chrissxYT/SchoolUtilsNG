8c5914ebd017a2e88a66e7a5c860233cabade0e4
chrissx@ThinkBadly <chrissx@chrissx.de>
2020-02-09 00:33:51 +0100

    finished a primitive server-implementation

--- backend_server/server.go
+++ backend_server/server.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
 )
 
 func run() error {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
-			bfr := make([]byte, 1024*1024*16)
-			n, err := r.Body.Read(bfr)
-			if err != nil {
-				log.Fatal(err)
+			bfr := []byte(r.URL.Path)
+			fn := fmt.Sprintf("%8x", rand.Int31())
+			for _, err := os.Stat(fn); !os.IsNotExist(err); {
+				fn = fmt.Sprintf("%8x", rand.Int31())
 			}
-			//write bfr to a file
+			err := ioutil.WriteFile(fn, bfr, 0644)
+			if err != nil {
+				log.Print(err)
+				fmt.Fprintf(w, "An error occured: %v", err)
+				return
+			}
+			log.Printf("Saved file %s successfully.", fn)
+			fmt.Fprintf(w, "Saved file %s successfully.", fn)
+		} else {
+			fmt.Fprintf(w, "This is an empty page.")
 		}
-		fmt.Fprintf(w, "This is an empty page.")
 	})
 
 	http.ListenAndServe(":4269", nil)
